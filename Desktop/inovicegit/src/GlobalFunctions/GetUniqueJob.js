89c2a3a9fe08cb4ce13391f20f5dc9807b98bc0d
User <optigoapi@gmail.com>
2023-11-27 12:16:23 +0530

    6A changes

--- Desktop/inovicegit/src/GlobalFunctions/GetUniqueJob.js
+++ Desktop/inovicegit/src/GlobalFunctions/GetUniqueJob.js
@@ -1,8 +1,8 @@
 export const GetUniquejob = (str_srjobno) => {
   let job = str_srjobno;
   const parts = job?.split(",");
-  const revParts = parts?.reverse();
-  const jobs = [...new Set(revParts)];
+  // const revParts = parts?.reverse();
+  const jobs = [...new Set(parts)];
   const resultString = jobs?.map((part) => `'${part}'`)?.join(",");
   return resultString;
 };
