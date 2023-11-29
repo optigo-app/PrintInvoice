7fdaa1bd102d3215f4d88c350a093325bc9de2c6
User <optigoapi@gmail.com>
2023-11-29 17:42:42 +0530

    bg 20 fs change

--- Desktop/inovicegit/src/GlobalFunctions/setFinalArr.js
+++ Desktop/inovicegit/src/GlobalFunctions/setFinalArr.js
@@ -1,22 +1,28 @@
-export const setFinalArr = (semiFinalArr) => {
-    console.log(semiFinalArr);
+export const setFinalArr = (semiFinalArr, arr, arr1, arr2) => {
     let finalArr = [];
     semiFinalArr?.forEach((e, i) => {
-            console.log(e);
-            if(e?.GroupJob === ''){
-                finalArr.push(e);
+        
+        if(e?.GroupJob === ''){
+            finalArr.push(e)
+        }else{
+            let obj = {...e};
+            let findRecord = finalArr?.findIndex((elem) => elem?.GroupJob === elem?.SrJobno);
+            if(findRecord === -1){
+                finalArr.push(obj);
             }else{
-                let obj = {...e};
-                let findRecord =  finalArr?.findIndex((e) => e?.GroupJob === obj.SrJobno);
-                if(findRecord === -1){
-                    finalArr.push(obj);
-                }else{
-                    if(obj.GroupJob !== obj.SrJobno){
-                        
-                    }
+                if(obj.GroupJob !== obj.SrJobno){
+
+                    console.log(finalArr[findRecord]);
+                    // obj.SrJobno = finalArr[findRecord].SrJobno;
+                    // obj.DesignImage = finalArr[findRecord].DesignImage;
+                    // obj.HUID = finalArr[findRecord].HUID;
+                    // obj.designno = finalArr[findRecord].designno;
+                    // obj.CertificateNo = finalArr[findRecord].CertificateNo;
+                    // obj.JewelCodePrefix = finalArr[findRecord].JewelCodePrefix;
                 }
-                console.log(finalArr[findRecord]);
             }
+            
+        }
     })
 
 
