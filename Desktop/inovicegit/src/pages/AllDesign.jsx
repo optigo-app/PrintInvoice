d57c6e55eb69d52044c366cdcb8dbb87c4f7b0d3
User <optigoapi@gmail.com>
2024-01-05 17:38:01 +0530

    grid set

--- Desktop/inovicegit/src/pages/AllDesign.jsx
+++ Desktop/inovicegit/src/pages/AllDesign.jsx
@@ -1,40 +1,41 @@
-import React, { useEffect, Suspense, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, Suspense, useState } from "react";
+import { useLocation } from "react-router-dom";
 // import AllDesignBagPrint from './AllDeisgnBagPrint';
-import AllDesignPrint from './AllDesignPrint';
-import AllDesignBagPrint2 from './AllDesignBagPrint2';
-import AllGrids from './AllGrids';
+import AllDesignPrint from "./AllDesignPrint";
+import AllDesignBagPrint2 from "./AllDesignBagPrint2";
+import AllGrids from "./AllGrids";
 
 const AllDesign = () => {
   const location = useLocation();
-  const queryParamss = new URLSearchParams(location.search);
+  // const queryParams = new URLSearchParams(location.search);
   const [loadedComponent, setLoadedComponent] = useState(null);
 
-  // const openProject = async () => {
-  //   if (queryParamss.get('printname')) {
-  //     return import('./AllDesignBagPrint2');
-  //   } else if (queryParamss.get('pnm')) {
-  //     return import('./AllDesignPrint');
-  //   } else if (queryParamss.get('grids')) {
-  //     return import('./AllGrids');
-  //   }
-  // };
+  const openProject = (searchUrl) => {
+    if (searchUrl?.includes("pnm")) {
+      return <AllDesignPrint />;
+    } else if (searchUrl?.includes("printname")) {
+      return <AllDesignBagPrint2 />;
+    } else if (searchUrl?.includes("grids")) {
+      return <AllGrids />;
+    } else {
+      return null;
+    }
+    // console.log(atob(queryParams.get('pnm')));
+  };
+  useEffect(() => {
+    const component = openProject(location.search);
+    setLoadedComponent(component);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  // useEffect(() => {
-  //   const loadComponent = async () => {
-  //     const component = await openProject();
-  //     setLoadedComponent(component.default);
-  //   };
-
-  //   loadComponent();
-  // }, [queryParamss]);
   return (
     <>
-      {queryParamss.get('printname') && <AllDesignBagPrint2 />}
-      {queryParamss.get('pnm') && <AllDesignPrint />}
-      {queryParamss.get('grids') && <AllGrids />}
+      {loadedComponent}
+      {/* {queryParams.get('printname') && <AllDesignBagPrint2 />}
+      {queryParams.get('pnm') && <AllDesignPrint />}
+      {queryParams.get('grids') && <AllGrids />} */}
     </>
-  )
-}
+  );
+};
 
-export default AllDesign
+export default AllDesign;
