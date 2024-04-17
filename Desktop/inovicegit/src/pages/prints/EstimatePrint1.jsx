a4c4ef1ca35145db411358a1bf070efeda56eeb2
optigo-app <optigoapi@gmail.com>
2024-04-17 15:27:51 +0530

    changes

--- Desktop/inovicegit/src/pages/prints/EstimatePrint1.jsx
+++ Desktop/inovicegit/src/pages/prints/EstimatePrint1.jsx
@@ -91,13 +91,13 @@ const EstimatePrint1 = ({ token, invoiceNo, printName, urls, evn, ApiVer }) => {
                         }
                     }
                     else if (ele?.MasterManagement_DiamondStoneTypeid === 3) {
-                       if(ele?.IsHSCOE === 0 || ele?.IsHSCOE === 3){
-                        if (ele?.Rate !== 0) {
+                    //    if(ele?.IsHSCOE === 0 || ele?.IsHSCOE === 3){
+                        if (ele?.Amount !== 0) {
                             totals.miscWt += ele?.Wt;
                             totals.OtherCharges += ele?.Amount;
                             misc.push(ele);
                         }
-                       }
+                    //    }
                     }
                 }
             });
