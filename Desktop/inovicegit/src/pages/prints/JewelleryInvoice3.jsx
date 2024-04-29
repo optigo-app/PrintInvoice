de8513d28a934cf5d6df506ea19ad11c5ac2ab5b
User <optigoapi@gmail.com>
2024-04-29 15:40:35 +0530

    known data fitted in ji3

--- Desktop/inovicegit/src/pages/prints/JewelleryInvoice3.jsx
+++ Desktop/inovicegit/src/pages/prints/JewelleryInvoice3.jsx
@@ -77,7 +77,7 @@ console.log(result);
             {
                 msg === '' ? <>
                 <div className='containerji3'>
-                    <div className='d-flex w-100 mt-5 mb-5 end_ji3'><Button /></div>
+                    <div className='d-flex w-100 mt-5 mb-5 end_ji3 d_none_ji3'><Button /></div>
                     <div className='p-1 d-flex justify-content-between align-items-center'>
                         <div>                    
                             <div className='my-3'>*ESTIMATE[SILVER]*</div>
@@ -86,7 +86,7 @@ console.log(result);
                         </div>
                         <div className='img_ji3'><img src={result?.header?.PrintLogo} alt="#clogo" className='clogo_ji3' /></div>
                     </div>
-                    <div className='mt-1 brall_ji3  d-flex justify-content-between align-items-start w-100'>
+                    <div className='mt-1 brall_ji3  d-flex justify-content-between align-items-start w-100 '>
                         <div className='fs_ji3 fw-bold p-1 w-75'>To, {result?.header?.customerfirmname}</div>
                         <div className='brleft_ji3 w-25'>
                             <div className='fs_ji3 p-1 py-2 brbottom_ji3'>BillNo : <span className='fw-bold '>{result?.header?.InvoiceNo}</span> / No : <span className='fw-bold'>1</span></div>
@@ -124,12 +124,13 @@ console.log(result);
                         <div className='tbody_ji3'>
                             {
                                 result?.resultArray?.map((e, i) => {
+                                    console.log(e);
                                     return (
                                         <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3'>
                                             <div className='col1t_ji3 brright_ji3 center_ji3'>{i+1}</div>
-                                            <div className='col2t_ji3 brright_ji3 center_ji3'>SKU</div>
-                                            <div className='col3t_ji3 brright_ji3 center_ji3'>Gross Weight</div>
-                                            <div className='col4t_ji3 brright_ji3 center_ji3'>Net Weight</div>
+                                            <div className='col2t_ji3 brright_ji3 start_ji3 ps-1'>{e?.designno}</div>
+                                            <div className='col3t_ji3 brright_ji3 end_ji3 pe-1'>{e?.grosswt?.toFixed(3)}</div>
+                                            <div className='col4t_ji3 brright_ji3 end_ji3 pe-1'>{e?.NetWt?.toFixed(3)}</div>
                                             <div className='col5t_ji3 brright_ji3 center_ji3'>Pcs</div>
                                             <div className='col6t_ji3 brright_ji3 center_ji3'>Wt</div>
                                             <div className='col7t_ji3 brright_ji3 center_ji3'>Pcs</div>
@@ -154,10 +155,10 @@ console.log(result);
                                         </div>
                         </div>
                     </div>
-                    <div className='mt-3 d-flex justify-content-end'>
+                    <div className='mt-3 d-flex justify-content-end '>
                         <div className='w-50 brtop_ji3'>
 
-                            <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3'>
+                            <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3 pbia_ji3'>
                                 <div className='col1acc_ji3 brright_ji3 start_ji3'>Labour Charge</div>
                                 <div className='col2acc_ji3 brright_ji3 end_ji3 pe-1'>2000</div>
                                 <div className='col3acc_ji3 brright_ji3 end_ji3 pe-1'>1000</div>
@@ -165,7 +166,7 @@ console.log(result);
                             </div>
                             {
                                 othDetails?.map((e, i) => {
-                                    return  <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3' key={i}>
+                                    return  <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3 pbia_ji3' key={i}>
                                             <div className='col1acc_ji3 brright_ji3'></div>
                                             <div className='col2acc_ji3 brright_ji3 pe-1 end_ji3 '>{e?.label}</div>
                                             <div className='col3acc_ji3 brright_ji3 pe-1 end_ji3 '>{e?.amtval}</div>
@@ -179,7 +180,7 @@ console.log(result);
                                         <React.Fragment key={i}>
                                         {
                                             e?.diamonds?.map((a, ind) => {
-                                                return  <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3' key={ind}>
+                                                return  <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3 pbia_ji3' key={ind}>
                                                     <div className='col1acc_ji3 brright_ji3'></div>
                                                     <div className='col2acc_ji3 brright_ji3 pe-1 end_ji3 '>{a?.Rate}</div>
                                                     <div className='col3acc_ji3 brright_ji3 pe-1 end_ji3 '>{a?.Wt?.toFixed(3)} (ctw)</div>
@@ -198,7 +199,7 @@ console.log(result);
                                         <React.Fragment key={i2}>
                                         {
                                             e?.colorstone?.map((a, ind) => {
-                                                return  <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3' key={ind}>
+                                                return  <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3 pbia_ji3' key={ind}>
                                                     <div className='col1acc_ji3 brright_ji3'></div>
                                                     <div className='col2acc_ji3 brright_ji3 pe-1 end_ji3 '>{a?.Rate}</div>
                                                     <div className='col3acc_ji3 brright_ji3 pe-1 end_ji3 '>{a?.Wt?.toFixed(3)} (ctw)zzzzzz</div>
@@ -217,7 +218,7 @@ console.log(result);
                                         <React.Fragment key={i2}>
                                         {
                                             e?.misc?.map((a, ind) => {
-                                                return  <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3' key={ind}>
+                                                return  <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3 pbia_ji3' key={ind}>
                                                     <div className='col1acc_ji3 brright_ji3'></div>
                                                     <div className='col2acc_ji3 brright_ji3 pe-1 end_ji3 '>{a?.Rate}</div>
                                                     <div className='col3acc_ji3 brright_ji3 pe-1 end_ji3 '>{a?.Wt?.toFixed(3)} (ctw)zzzzzz</div>
@@ -236,7 +237,7 @@ console.log(result);
                                         <React.Fragment key={i2}>
                                         {
                                             e?.finding?.map((a, ind) => {
-                                                return  <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3' key={ind}>
+                                                return  <div className='d-flex brright_ji3 brleft_ji3 brbottom_ji3 fs_ji3 pbia_ji3' key={ind}>
                                                     <div className='col1acc_ji3 brright_ji3'></div>
                                                     <div className='col2acc_ji3 brright_ji3 pe-1 end_ji3 '>{a?.Rate}</div>
                                                     <div className='col3acc_ji3 brright_ji3 pe-1 end_ji3 '>{a?.Wt?.toFixed(3)} (ctw)</div>
@@ -280,11 +281,11 @@ console.log(result);
                            
                         </div>
                     </div>
-                    <div className='mt-2 p-1 brall_ji3 fs_ji3 d-flex align-items-center'>
+                    <div className='mt-2 p-1 brall_ji3 fs_ji3 d-flex align-items-center pbia_ji3'>
                         <div className='fw-bold text-decoration-underline'>Remark :</div>
                         <div className='px-2'>{result?.header?.PrintRemark}</div>
                     </div>
-                    <div className='d-flex mt-2 fs_ji3 justify-content-between align-items-center'>
+                    <div className='d-flex mt-2 fs_ji3 justify-content-between align-items-center pbia_ji3'>
                         <div className='d-flex w-50'><div>Receiver's Signature : </div><div className='w-25 border-black border-bottom'></div></div>
                         <div className='d-flex justify-content-end w-50'><div>Sign:</div><div className='w-25 border-bottom border-black'></div></div>
                     </div>
