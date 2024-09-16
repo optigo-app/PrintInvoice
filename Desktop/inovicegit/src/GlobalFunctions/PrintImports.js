dc4559ef39d218749505f2e66a27ad51f0fd96ea
User <optigoapi@gmail.com>
2024-09-16 15:07:09 +0530

    event set up

--- Desktop/inovicegit/src/GlobalFunctions/PrintImports.js
+++ Desktop/inovicegit/src/GlobalFunctions/PrintImports.js
@@ -146,6 +146,7 @@ export const QuotationPrints = [
   // {printName: 'summary 4', etpType: 'print', componentName: 'Summary4'},
   {printName: 'jewellery tax invoice', etpType: 'print', componentName: 'JewelleryTaxInvoice'},
   {printName: 'packing list', etpType: 'print', componentName: 'PackingList'},
+  {printName: 'packing list 1', etpType: 'print', componentName: 'PackingList1'},
   {printName: 'summary 4', etpType: 'print', componentName: 'Summary4Quote'},
   {printName: 'summary print', etpType: 'print', componentName: 'SummaryPrint'},
   {printName: 'detail print 12', etpType: 'print', componentName: 'DetailPrint12Quote'},
@@ -165,6 +166,8 @@ export const MemoPrints = [
   {printName: 'packing list 7group', etpType: 'print', componentName: 'PackingList7Group'},
   {printName: 'summary 12', etpType: 'print', componentName: 'Summary12'},
   {printName: 'estimate print', etpType: 'print', componentName: 'EstimatePrint'},
+  {printName: 'packing list', etpType: 'print', componentName: 'PackingList'},
+  {printName: 'packing list 1', etpType: 'print', componentName: 'PackingList1'},
   {printName: 'packing list 3', etpType: 'print', componentName: 'PackingList3'},
 ]
 export const MemoReturnPrints = [ 
@@ -186,6 +189,8 @@ export const EstimatePrints = [
   {printName: 'packing list 7group', etpType: 'print', componentName: 'PackingList7Group'},
   {printName: 'detail print 12', etpType: 'print', componentName: 'DetailPrint12'},
   {printName: 'sale order', etpType: 'print', componentName: 'SaleOrderSale'},
+  {printName: 'packing list', etpType: 'print', componentName: 'PackingList'},
+  {printName: 'packing list 1', etpType: 'print', componentName: 'PackingList1'},
 ]
 export const HallMarkPrints = [
   {printName: 'jewellery item wise', etpType: 'print', componentName: 'HallmarkItemWisePrint'},
