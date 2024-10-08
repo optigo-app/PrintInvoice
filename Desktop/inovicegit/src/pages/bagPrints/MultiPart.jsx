f1b84d4e9fadb61ac17c23d9a05d1e2c4471bb66
User <optigoapi@gmail.com>
2024-10-08 14:43:02 +0530

    ui changes

--- Desktop/inovicegit/src/pages/bagPrints/MultiPart.jsx
+++ Desktop/inovicegit/src/pages/bagPrints/MultiPart.jsx
@@ -190,34 +190,80 @@ const MultiPart = ({ queries, headers }) => {
   
 
   return (
-    <div>
+    <div className='bg_color_mlt'>
     {
       data?.length === 0 ? <Loader /> : <React.Fragment>
         {/* <div className="print_btn"><button className="btn_white blue print_btn" onClick={(e) => handlePrint(e)}>
                 Print
             </button></div> */}
-            <div className='hideOnPrint'>
+            <div className='hideOnPrint bg_color_container h_100_vh_mlt'>
                 <div className='w-100 d-flex align-items-center justify-content-center'>
-                    <div className='multipart_container p-1'>
+                    <div className=' d-flex flex-column justify-content-between align-items-center p-1'>
                         <div className='d-flex align-items-center justify-content-center py-2'>
-                            <div className='multipart_head'>Multi Part Bagging Total Jobs : </div>
-                            <div className='multipart_head text-center' style={{minWidth:'100px'}}>{data?.length}</div>
+                            <div className='multipart_head' style={{fontFamily:'Helvetica, Verdana, sans-serif'}}>Multi Part Bagging Process For &nbsp;</div>
+                            <div className='multipart_head text-center number-box border border_color_head' style={{minWidth:'100px'}}>{data?.length}</div>
+                            <div className='multipart_head'>&nbsp;{data?.length > 1 ? 'Jobs' : 'Job'}</div>
                         </div>
-                        <div className='d-flex justify-content-between align-items-center py-2'>
-                        <div style={{ margin: "1rem" }}>
+                        <div className='d-flex  justify-content-between align-items-center py-2 flex-wrap'>
+                            <div style={{margin:'1rem', boxSizing:'border-box'}} className='bg-white'>
+                                <Box className="border rounded box_css_mlt d-flex flex-column border_color_head justify-content-between align-items-center ">
+                                    <div className='w-100 d-flex justify-content-center align-items-center py-2 border-bottom border_color_head'><Typography sx={{fontWeight:'bold'}}>DIAMOND BAGS</Typography></div>
+                                    <div className='p-2 text-break text-center'>
+                                            Your Selection having <span className='number-box border border_color_head'>{countObj?.dia}</span>  <span className='fw-bold'>Jobs</span> for <span className='fw-bold'>Diamond </span> bagging.
+                                    </div>
+
+                                    <div className='p-1 d-flex justify-content-center align-items-center pb-2'>
+                                        <Button variant='contained' size='small' color='primary' disabled={countObj.dia < 1} onClick={() => handleButtonClick('diamond')}>Print</Button>
+                                    </div>
+                                </Box>
+                            </div>
+                            <div style={{margin:'1rem', boxSizing:'border-box'}} className='bg-white'>
+                                <Box className="border rounded box_css_mlt d-flex flex-column border_color_head justify-content-between align-items-center ">
+                                    <div className='w-100 d-flex justify-content-center align-items-center py-2 border-bottom border_color_head'><Typography sx={{fontWeight:'bold'}}>COLORSTONE BAGS</Typography></div>
+                                    <div className='p-2 text-break text-center'>
+                                            Your Selection having <span className='number-box border border_color_head'>{countObj?.cls}</span>  <span className='fw-bold'>Jobs</span> for <span className='fw-bold'>Colorstone </span> bagging.
+                                    </div>
+
+                                    <div className='p-1 d-flex justify-content-center align-items-center pb-2'>
+                                        <Button variant='contained' size='small' color='primary' disabled={countObj.cls < 1} onClick={() => handleButtonClick('colorstone')}>Print</Button>
+                                    </div>
+                                </Box>
+                            </div>
+                            <div style={{margin:'1rem', boxSizing:'border-box'}} className='bg-white'>
+                                <Box className="border rounded box_css_mlt d-flex flex-column border_color_head justify-content-between align-items-center ">
+                                    <div className='w-100 d-flex justify-content-center align-items-center py-2 border-bottom border_color_head'><Typography sx={{fontWeight:'bold'}}>MISC BAGS</Typography></div>
+                                    <div className='p-2 text-break text-center'>
+                                            Your Selection having <span className='number-box border border_color_head'>{countObj?.misc}</span>  <span className='fw-bold'>Jobs</span> for <span className='fw-bold'>Misc </span> bagging.
+                                    </div>
+
+                                    <div className='p-1 d-flex justify-content-center align-items-center pb-2'>
+                                        <Button variant='contained' size='small' color='primary' disabled={countObj.misc < 1} onClick={() => handleButtonClick('misc')}>Print</Button>
+                                    </div>
+                                </Box>
+                            </div>
+                            <div style={{margin:'1rem', boxSizing:'border-box'}} className='bg-white'>
+                                <Box className="border rounded box_css_mlt d-flex flex-column border_color_head justify-content-between align-items-center ">
+                                    <div className='w-100 d-flex justify-content-center align-items-center py-2 border-bottom border_color_head'><Typography sx={{fontWeight:'bold'}}>FINDING BAGS</Typography></div>
+                                    <div className='p-2 text-break text-center'>
+                                            Your Selection having <span className='number-box border border_color_head'>{countObj?.finding}</span>  <span className='fw-bold'>Jobs</span> for <span className='fw-bold'>Finding </span> bagging.
+                                    </div>
+
+                                    <div className='p-1 d-flex justify-content-center align-items-center pb-2'>
+                                        <Button variant='contained' size='small' color='primary' disabled={countObj.finding < 1} onClick={() => handleButtonClick('finding')}>Print</Button>
+                                    </div>
+                                </Box>
+                            </div>
+                        {/* <div style={{ margin: "1rem" }}>
                             <Box className="diaCount diaextra">{countObj.dia}</Box>
                             <Button
                                 color="success"
                                 variant="contained"
-                                sx={{
-                                    fontWeight: "bold",
-                                    backgroundColor: `#28C76F`,
-                                    textTransform: "none",
-                                    minWidth:'120px'
-                                }}
+                                sx={{ fontWeight: "bold", backgroundColor: `#28C76F`, textTransform: "none", minWidth:'120px' }}
                                 className='succ_btn'
+                                disabled={countObj.dia < 1}
                                 onClick={() => handleButtonClick('diamond')}
                             >
+                                {console.log(countObj.dia)}
                                 <Typography sx={{color:'white', fontWeight:'bold'}}>Diamond</Typography>
                             </Button>
                         </div>
@@ -226,13 +272,8 @@ const MultiPart = ({ queries, headers }) => {
                             <Button
                                 color="success"
                                 variant="contained"
-                                sx={{
-                                    fontWeight: "bold",
-                                    backgroundColor: `#EA5455`,
-                                    textTransform: "none",
-                                    minWidth:'120px'
-                                }}
-                                className='cls_btn'
+                                sx={{ fontWeight: "bold", backgroundColor: `#EA5455`, textTransform: "none", minWidth:'120px' }} className='cls_btn'
+                                disabled={countObj.cls < 1}
                                 onClick={() => handleButtonClick('colorstone')}
                             >
                                 <Typography sx={{color:'white', fontWeight:'bold'}}>ColorStone</Typography>
@@ -243,13 +284,9 @@ const MultiPart = ({ queries, headers }) => {
                             <Button
                                 color="success"
                                 variant="contained"
-                                sx={{
-                                    fontWeight: "bold",
-                                    backgroundColor: `#FF9F43`,
-                                    textTransform: "none",
-                                    minWidth:'120px'
-                                }}
+                                sx={{ fontWeight: "bold", backgroundColor: `#FF9F43`, textTransform: "none", minWidth:'120px' }}
                                 className='misc_btn'
+                                disabled={countObj.misc < 1}
                                 onClick={() => handleButtonClick('misc')}
                             >
                                 <Typography sx={{color:'white', fontWeight:'bold'}}>Misc</Typography>
@@ -260,18 +297,14 @@ const MultiPart = ({ queries, headers }) => {
                             <Button
                                 color="success"
                                 variant="contained"
-                                sx={{
-                                    fontWeight: "bold",
-                                    backgroundColor: `#655BD3`,
-                                    textTransform: "none",
-                                    minWidth:'120px'
-                                }}
+                                sx={{ fontWeight: "bold", backgroundColor: `#655BD3`, textTransform: "none", minWidth:'120px' }}
                                 className='find_btn'
+                                disabled={countObj.finding < 1}
                                 onClick={() => handleButtonClick('finding')}
                             >
                                 <Typography sx={{color:'white', fontWeight:'bold'}}>Finding</Typography>
                             </Button>
-                        </div>
+                        </div> */}
                         </div>
                     </div>
                 </div>
