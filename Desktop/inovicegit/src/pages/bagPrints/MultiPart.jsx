a1e12c62df08b0cb6b861069e573a0836db58909
User <optigoapi@gmail.com>
2024-10-08 12:29:19 +0530

    added print done build uploading

--- Desktop/inovicegit/src/pages/bagPrints/MultiPart.jsx
+++ Desktop/inovicegit/src/pages/bagPrints/MultiPart.jsx
@@ -13,11 +13,11 @@ import { Box, Button, Typography } from "@mui/material";
 const MultiPart = ({ queries, headers }) => {
     const location = useLocation();
     const [filterData, setFilterData] = useState([]);
-    const [jobsData, setJobsData] = useState([]);
     const [countObj, setCountObj] = useState({});
     const queryParams = queryString.parse(location.search);
     const resultString = GetUniquejob(queryParams?.str_srjobno);
     const [data, setData] = useState([]);
+    const [title, setTitle] = useState('');
       
   useEffect(() => {
     const fetchData = async () => {
@@ -31,7 +31,11 @@ const MultiPart = ({ queries, headers }) => {
             dia:0,
             cls:0,
             misc:0,
-            finding:0
+            finding:0,
+            diamondJobList:[],
+            colorStoneList:[],
+            miscJobList:[],
+            findingJobList:[]
         }
         let arr = [];
         abc[0]?.allDatas?.rd1?.forEach((a) => {
@@ -57,20 +61,27 @@ const MultiPart = ({ queries, headers }) => {
                 //clr
             }
         })
-        const uniqueDiamondJobs = new Set(diaList.map(d => d.SerialJobno));
-        const uniqueClsJobs = new Set(clsList.map(d => d.SerialJobno));
-        const uniqueMiscJobs = new Set(miscList.map(d => d.SerialJobno));
-        const uniqueFindingJobs = new Set(findingList.map(d => d.SerialJobno));
+        const uniqueDiamondJobs = [...new Set(diaList.map(d => d.SerialJobno))];
+        const uniqueClsJobs = [...new Set(clsList.map(d => d.SerialJobno))];
+        const uniqueMiscJobs = [...new Set(miscList.map(d => d.SerialJobno))];
+        const uniqueFindingJobs = [...new Set(findingList.map(d => d.SerialJobno))];
         // Store the unique job count in obj.dia
-        obj.dia = uniqueDiamondJobs.size;
-        obj.cls = uniqueClsJobs.size;
-        obj.misc = uniqueMiscJobs.size;
-        obj.finding = uniqueFindingJobs.size;
+        obj.dia = uniqueDiamondJobs.length;
+        obj.diamondJobList = uniqueDiamondJobs;
+
+        obj.cls = uniqueClsJobs.length;
+        obj.colorStoneList = uniqueClsJobs;
+
+        obj.misc = uniqueMiscJobs.length;
+        obj.miscJobList = uniqueMiscJobs;
+
+        obj.finding = uniqueFindingJobs.length;
+        obj.findingJobList = uniqueFindingJobs;
 
         setCountObj(obj);
 
         // obj.dia = diaList?.length;
-        setJobsData(arr);
+        // setJobsData(arr);
         
         abc?.length > 0 ? setData(abc) : setData([])
       } catch (error) {
@@ -89,114 +100,195 @@ const MultiPart = ({ queries, headers }) => {
     }, [queryParams]);
 
     const handleButtonClick = (args) => {
-        console.log(args);
+        setTitle('');
+        setFilterData([]);
+        if(args === 'diamond'){
+            let finalArr = [];
+            data?.forEach((e, i) => {
+                countObj?.diamondJobList?.forEach((a) => {
+                    if(e?.data?.rd?.serialjobno === a){
+                        finalArr.push(e);
+                    }
+                })
+            })
+            setTimeout(() => {
+                setFilterData(finalArr);
+                setTitle('DIAMOND')
+
+                // setData(finalArr);
+            },0);
+            if(finalArr.length > 0){
+                setTimeout(() => {
+                    window.print();
+                }, 1000) 
+            }
+        }
+        if(args === 'colorstone'){
+            let finalArr = [];
+            data?.forEach((e, i) => {
+                countObj?.colorStoneList?.forEach((a) => {
+                    if(e?.data?.rd?.serialjobno === a){
+                        finalArr.push(e);
+                    }
+                })
+            })
+            setTimeout(() => {
+                setFilterData(finalArr);
+                setTitle('COLORSTONE')
+
+                // setData(finalArr);
+            },0);
+            if(finalArr.length > 0){
+                setTimeout(() => {
+                    window.print();
+                }, 1000) 
+            }
+        }
+        if(args === 'misc'){
+            let finalArr = [];
+            data?.forEach((e, i) => {
+                countObj?.miscJobList?.forEach((a) => {
+                    if(e?.data?.rd?.serialjobno === a){
+                        finalArr.push(e);
+                    }
+                })
+            })
+            setTimeout(() => {
+                setFilterData(finalArr);
+                setTitle('MISC')
+
+                // setData(finalArr);
+            },0);
+            if(finalArr.length > 0){
+                setTimeout(() => {
+                    window.print();
+                }, 1000) 
+            }
+        }
+        if(args === 'finding'){
+            let finalArr = [];
+            data?.forEach((e, i) => {
+                countObj?.findingJobList?.forEach((a) => {
+                    if(e?.data?.rd?.serialjobno === a){
+                        finalArr.push(e);
+                    }
+                })
+            })
+            setTimeout(() => {
+                setFilterData(finalArr);
+                setTitle('FINDING')
+
+                // setData(finalArr);
+            },0);
+            if(finalArr.length > 0){
+                setTimeout(() => {
+                    window.print();
+                }, 1000) 
+            }
+        }
     }
   
-//   useEffect(() => {
-//       if (data?.length !== 0) {
-//           setTimeout(() => {
-//               window.print();
-//           }, 4000);
-//       }
-//   }, [data]);
 
   return (
     <div>
     {
       data?.length === 0 ? <Loader /> : <React.Fragment>
-        <div className="print_btn"><button className="btn_white blue print_btn" onClick={(e) => handlePrint(e)}>
+        {/* <div className="print_btn"><button className="btn_white blue print_btn" onClick={(e) => handlePrint(e)}>
                 Print
-            </button></div>
-            <div className='w-100 d-flex align-items-center justify-content-center'>
-                <div className='multipart_container p-1'>
-                    <div className='d-flex align-items-center justify-content-center py-2'>
-                        <div className='multipart_head'>Multi Part Bagging Total Jobs : </div>
-                        <div className='multipart_head text-center' style={{minWidth:'100px'}}>{data?.length}</div>
-                    </div>
-                    <div className='d-flex justify-content-between align-items-center py-2'>
-                    <div style={{ margin: "1rem" }}>
-                        <Box className="diaCount diaextra">{countObj.dia}</Box>
-                        <Button
-                            color="success"
-                            variant="contained"
-                            sx={{
-                                fontWeight: "bold",
-                                backgroundColor: `#28C76F`,
-                                textTransform: "none",
-                                minWidth:'120px'
-                            }}
-                            className='succ_btn'
-                            onClick={() => handleButtonClick('diamond')}
-                        >
-                            <Typography sx={{color:'white', fontWeight:'bold'}}>Diamond</Typography>
-                        </Button>
-                    </div>
-                    <div style={{ margin: "1rem" }}>
-                        <Box className="diaCount diaextra1">{countObj.cls}</Box>
-                        <Button
-                            color="success"
-                            variant="contained"
-                            sx={{
-                                fontWeight: "bold",
-                                backgroundColor: `#EA5455`,
-                                textTransform: "none",
-                                minWidth:'120px'
-                            }}
-                            className='cls_btn'
-                            onClick={() => handleButtonClick('colorstone')}
-                        >
-                            <Typography sx={{color:'white', fontWeight:'bold'}}>ColorStone</Typography>
-                        </Button>
-                    </div>
-                    <div style={{ margin: "1rem" }}>
-                        <Box className="diaCount diaextra2">{countObj.misc}</Box>
-                        <Button
-                            color="success"
-                            variant="contained"
-                            sx={{
-                                fontWeight: "bold",
-                                backgroundColor: `#FF9F43`,
-                                textTransform: "none",
-                                minWidth:'120px'
-                            }}
-                            className='misc_btn'
-                            onClick={() => handleButtonClick('misc')}
-                        >
-                            <Typography sx={{color:'white', fontWeight:'bold'}}>Misc</Typography>
-                        </Button>
-                    </div>
-                    <div style={{ margin: "1rem" }}>
-                        <Box className="diaCount diaextra3">{countObj.finding}</Box>
-                        <Button
-                            color="success"
-                            variant="contained"
-                            sx={{
-                                fontWeight: "bold",
-                                backgroundColor: `#655BD3`,
-                                textTransform: "none",
-                                minWidth:'120px'
-                            }}
-                            className='find_btn'
-                            onClick={() => handleButtonClick('finding')}
-                        >
-                            <Typography sx={{color:'white', fontWeight:'bold'}}>Finding</Typography>
-                        </Button>
-                    </div>
+            </button></div> */}
+            <div className='hideOnPrint'>
+                <div className='w-100 d-flex align-items-center justify-content-center'>
+                    <div className='multipart_container p-1'>
+                        <div className='d-flex align-items-center justify-content-center py-2'>
+                            <div className='multipart_head'>Multi Part Bagging Total Jobs : </div>
+                            <div className='multipart_head text-center' style={{minWidth:'100px'}}>{data?.length}</div>
+                        </div>
+                        <div className='d-flex justify-content-between align-items-center py-2'>
+                        <div style={{ margin: "1rem" }}>
+                            <Box className="diaCount diaextra">{countObj.dia}</Box>
+                            <Button
+                                color="success"
+                                variant="contained"
+                                sx={{
+                                    fontWeight: "bold",
+                                    backgroundColor: `#28C76F`,
+                                    textTransform: "none",
+                                    minWidth:'120px'
+                                }}
+                                className='succ_btn'
+                                onClick={() => handleButtonClick('diamond')}
+                            >
+                                <Typography sx={{color:'white', fontWeight:'bold'}}>Diamond</Typography>
+                            </Button>
+                        </div>
+                        <div style={{ margin: "1rem" }}>
+                            <Box className="diaCount diaextra1">{countObj.cls}</Box>
+                            <Button
+                                color="success"
+                                variant="contained"
+                                sx={{
+                                    fontWeight: "bold",
+                                    backgroundColor: `#EA5455`,
+                                    textTransform: "none",
+                                    minWidth:'120px'
+                                }}
+                                className='cls_btn'
+                                onClick={() => handleButtonClick('colorstone')}
+                            >
+                                <Typography sx={{color:'white', fontWeight:'bold'}}>ColorStone</Typography>
+                            </Button>
+                        </div>
+                        <div style={{ margin: "1rem" }}>
+                            <Box className="diaCount diaextra2">{countObj.misc}</Box>
+                            <Button
+                                color="success"
+                                variant="contained"
+                                sx={{
+                                    fontWeight: "bold",
+                                    backgroundColor: `#FF9F43`,
+                                    textTransform: "none",
+                                    minWidth:'120px'
+                                }}
+                                className='misc_btn'
+                                onClick={() => handleButtonClick('misc')}
+                            >
+                                <Typography sx={{color:'white', fontWeight:'bold'}}>Misc</Typography>
+                            </Button>
+                        </div>
+                        <div style={{ margin: "1rem" }}>
+                            <Box className="diaCount diaextra3">{countObj.finding}</Box>
+                            <Button
+                                color="success"
+                                variant="contained"
+                                sx={{
+                                    fontWeight: "bold",
+                                    backgroundColor: `#655BD3`,
+                                    textTransform: "none",
+                                    minWidth:'120px'
+                                }}
+                                className='find_btn'
+                                onClick={() => handleButtonClick('finding')}
+                            >
+                                <Typography sx={{color:'white', fontWeight:'bold'}}>Finding</Typography>
+                            </Button>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            {/* <div className='mainjobbagsticker'>
+            <div className='mainjobbagsticker showOnPrint'>
             {Array.from({ length: queries?.pageStart }, (_, index) => (
                     index > 0 && <div key={index} className="container  ml_5 mb_10"></div>
                 ))}
                 {
-                  data?.length > 0 && data?.map((e, index) => {
+                  filterData?.length > 0 && filterData?.map((e, index) => {
                     return(
                       <React.Fragment key={index}>
                         {
                           e?.additional?.pages?.length > 0 ? e?.additional?.pages?.map((e, i) => {
                             return(
                               <div className='containerjbsbg' key={i}>
+                                <div className=' fsjbsbg fw-bold py-1 d-flex align-items-center'>{title}</div>
                             { e?.data?.rd?.serialjobno?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.serialjobno}</div>}
                             { e?.data?.rd?.Designcode?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.Designcode}</div>}
                             { e?.data?.rd?.CustomerCode?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.CustomerCode}</div>}
@@ -217,6 +309,7 @@ const MultiPart = ({ queries, headers }) => {
                           }) : 
                           
                           <div className='containerjbsbg'>
+                                <div className=' fsjbsbg fw-bold py-1 d-flex align-items-center'>{title}</div>
                             { e?.data?.rd?.serialjobno?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.serialjobno}</div>}
                             { e?.data?.rd?.Designcode?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.Designcode}</div>}
                             { e?.data?.rd?.CustomerCode?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.CustomerCode}</div>}
@@ -237,7 +330,7 @@ const MultiPart = ({ queries, headers }) => {
                     )
                   })
                 }
-            </div> */}
+            </div>
       </React.Fragment>
     }
 </div>
