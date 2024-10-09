91f533347bc2bcdbf933b673a3bed5a8deadc96d
User <optigoapi@gmail.com>
2024-10-09 12:02:32 +0530

    maintanance done (not done is reducing function)

--- Desktop/inovicegit/src/pages/bagPrints/MultiPart.jsx
+++ Desktop/inovicegit/src/pages/bagPrints/MultiPart.jsx
@@ -1,3 +1,157 @@
+// import React, { useEffect, useState } from 'react';
+// import { useLocation } from 'react-router-dom';
+// import queryString from 'query-string';
+// import "../../assets/css/bagprint/multipart.css";
+// // import "../../assets/css/bagprint/jobbagsticker.css";
+// import Loader from '../../components/Loader';
+// import QRCodeGenerator from '../../components/QRCodeGenerator';
+// import { GetUniquejob } from '../../GlobalFunctions/GetUniqueJob';
+// import { FetchDatas } from '../../GlobalFunctions/FetchDatas';
+// import { Box, Button, Typography } from "@mui/material";
+
+// const JobBagDetails = ({ jobData, title }) => (
+//   <div className='containerjbsbg'>
+//     <div className='fsjbsbg py-1 d-flex align-items-center'>{title}</div>
+//     {jobData?.data?.rd && (
+//       <>
+//         {Object.entries(jobData?.data?.rd).map(([key, value]) =>
+//           value && (
+//             <div className='fsjbsbg' key={key}>
+//               {key === 'MetalWeight' ? (+value).toFixed(3) + "gm" : value}
+//             </div>
+//           )
+//         )}
+//         <div className='d-flex justify-content-start align-items-center'>
+//           <QRCodeGenerator text={jobData?.data?.rd.serialjobno} />
+//         </div>
+//       </>
+//     )}
+//   </div>
+// );
+
+// const MultiPart = ({ queries, headers }) => {
+//   const location = useLocation();
+//   const queryParams = queryString.parse(location.search);
+//   const resultString = GetUniquejob(queryParams?.str_srjobno);
+//   const [data, setData] = useState([]);
+//   const [countObj, setCountObj] = useState({});
+//   const [filterData, setFilterData] = useState([]);
+//   const [title, setTitle] = useState('');
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const abc = await FetchDatas(queryParams, resultString, queries, headers);
+//         const obj = {
+//           diamond: 0, colorStone: 0, misc: 0, finding: 0,
+//           diamondJobList: [], colorStoneList: [],
+//           miscJobList: [], findingJobList: [],
+//         };
+//         abc[0]?.allDatas?.rd1?.forEach((a) => {
+//           const typeId = a?.MasterManagement_DiamondStoneTypeid;
+//           if (typeId === 5) { obj.findingJobList.push(a); }
+//           else if (typeId === 7) { obj.miscJobList.push(a); }
+//           else if (typeId === 3) { obj.diamondJobList.push(a); }
+//           else if (typeId === 4) { obj.colorStoneList.push(a); }
+//         });
+//         obj.diamondJobList = [...new Set(obj.diamondJobList.map(d => d.SerialJobno))];
+//         obj.diamond = obj.diamondJobList.length;
+//         obj.colorStoneList = [...new Set(obj.colorStoneList.map(d => d.SerialJobno))];
+//         obj.colorStone = obj.colorStoneList.length;
+//         obj.miscJobList = [...new Set(obj.miscJobList.map(d => d.SerialJobno))];
+//         obj.misc = obj.miscJobList.length;
+//         obj.findingJobList = [...new Set(obj.findingJobList.map(d => d.SerialJobno))];
+//         obj.finding = obj.findingJobList.length;
+
+//         // setCountObj(obj);    
+//         setTimeout(() => {
+//             setCountObj(obj);
+//         },10)
+//         setData(abc.length > 0 ? abc : []);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   const handleButtonClick = (type) => {
+//     setTitle('');
+//     setFilterData([]);
+//     const types = {
+//       diamond: { list: 'diamondJobList', title: 'DIAMOND' },
+//       colorstone: { list: 'colorStoneList', title: 'COLORSTONE' },
+//       misc: { list: 'miscJobList', title: 'MISC' },
+//       finding: { list: 'findingJobList', title: 'FINDING' },
+//     };
+//     if (types[type]) {
+//         const finalArr = [];
+//        data?.forEach(e => { 
+//         countObj[types[type]?.list]?.forEach((a, i) => {
+//             if(a === e?.data?.rd?.serialjobno){
+//                 finalArr.push(e);
+//             }
+//         })
+//       });
+//       console.log(finalArr);
+//       setFilterData(finalArr);
+//       setTitle(types[type].title);
+//       if (-1 > 0) {
+//         setTimeout(() => window.print(), 500);
+//       }
+//     }
+//   };
+
+//   return (
+//     <div className='bg_color_mlt pb-5 mb-5'>
+//       {data.length === 0 ? <Loader /> : (
+//         <div className='hideOnPrint'>
+//           <div className='w-100 d-flex align-items-center justify-content-center'>
+//             <div className='d-flex flex-column justify-content-between align-items-center p-1 w-100'>
+//               <div className='d-flex align-items-center justify-content-center py-2'>
+//                 <div className='multipart_head' style={{ fontFamily: 'Helvetica, Verdana, sans-serif' }}>Multi Part Bagging Process For &nbsp;</div>
+//                 <div className='multipart_head text-center number-box border border_color_head' style={{ minWidth: '100px' }}>{data.length}</div>
+//                 <div className='multipart_head'>&nbsp;{data.length > 1 ? 'Jobs' : 'Job'}</div>
+//               </div>
+//               <div className='d-flex justify-content-around align-items-center py-2 flex-wrap border bg_color_container' style={{ marginTop: '3%', minHeight: '30rem', width: '80%', padding: '1%' }}>
+//                 {['diamond', 'colorStone', 'misc', 'finding'].map(type => (
+//                   <div key={type} style={{ margin: '1rem', boxSizing: 'border-box' }} className='bg-white text-black mx-0'>
+//                     <Box className="border rounded box_css_mlt d-flex flex-column border_color_head justify-content-between align-items-center m-0">
+//                       <div className='w-100 d-flex justify-content-center align-items-center py-2 border-bottom border_color_head'>
+//                         <Typography sx={{ fontWeight: 'bold' }}>{`${type.toUpperCase()} BAGS`}</Typography>
+//                       </div>
+//                       {console.log(countObj)}
+//                       <div className='countBox'>{countObj[type]}</div>
+//                       <div>Jobs Are Ready To Print</div>
+//                       <div className='p-1 d-flex justify-content-center align-items-center pb-3'>
+//                         <button className="btn_white btn_w2 blue print_btn" disabled={countObj[type] < 1} onClick={() => handleButtonClick(type)}>
+//                           Print
+//                         </button>
+//                       </div>
+//                     </Box>
+//                   </div>
+//                 ))}
+//               </div>
+//             </div>
+//           </div>
+//           <div className='mainjobbagsticker showOnPrint'>
+//             {Array?.from({ length: queries?.pageStart }, (_, index) => index > 0 && <div key={index} className="container ml_5 mb_10"></div>)}
+//             {filterData?.length > 0 && filterData?.map((e, index) => (
+//               <React.Fragment key={index}>
+//                 {e?.additional?.pages?.length > 0
+//                   ? e?.additional?.pages?.map((page, i) => <JobBagDetails key={i} jobData={page} title={title} />)
+//                   : <JobBagDetails jobData={e} title={title} />}
+//               </React.Fragment>
+//             ))}
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default MultiPart;
+
 import React from 'react'
 import "../../assets/css/bagprint/multipart.css";
 import queryString from 'query-string';
@@ -65,7 +219,7 @@ const MultiPart = ({ queries, headers }) => {
         const uniqueClsJobs = [...new Set(clsList.map(d => d.SerialJobno))];
         const uniqueMiscJobs = [...new Set(miscList.map(d => d.SerialJobno))];
         const uniqueFindingJobs = [...new Set(findingList.map(d => d.SerialJobno))];
-        // Store the unique job count in obj.dia
+
         obj.dia = uniqueDiamondJobs.length;
         obj.diamondJobList = uniqueDiamondJobs;
 
@@ -79,9 +233,6 @@ const MultiPart = ({ queries, headers }) => {
         obj.findingJobList = uniqueFindingJobs;
 
         setCountObj(obj);
-
-        // obj.dia = diaList?.length;
-        // setJobsData(arr);
         
         abc?.length > 0 ? setData(abc) : setData([])
       } catch (error) {
@@ -163,9 +314,6 @@ const MultiPart = ({ queries, headers }) => {
                                     <div className='w-100 d-flex justify-content-center align-items-center py-2 border-bottom border_color_head'><Typography sx={{fontWeight:'bold'}}>DIAMOND BAGS</Typography></div>
                                         <div className='countBox'>{countObj?.dia}</div>
                                         <div>Jobs Are Ready To Print</div>
-                                    {/* <div className='p-2 text-break text-center'>
-                                            Your Selection having <span className='number-box border border_color_head'>{countObj?.dia}</span>  <span >Jobs</span> <br></br> for <span >Diamond </span> bagging.
-                                    </div> */}
                                     <div className='p-1 d-flex justify-content-center align-items-center pb-3'>
                                         <button className="btn_white btn_w2 blue print_btn" disabled={countObj.dia < 1} onClick={() => handleButtonClick('diamond')}>Print</button>
                                     </div>
@@ -174,9 +322,6 @@ const MultiPart = ({ queries, headers }) => {
                             <div style={{margin:'1rem', boxSizing:'border-box'}} className='bg-white text-black mx-0'>
                                 <Box className="border rounded box_css_mlt d-flex flex-column border_color_head justify-content-between align-items-center m-0">
                                     <div className='w-100 d-flex justify-content-center align-items-center py-2 border-bottom border_color_head'><Typography sx={{fontWeight:'bold'}}>COLORSTONE BAGS</Typography></div>
-                                    {/* <div className='p-2 text-break text-center'>
-                                            Your Selection having <span className='number-box border border_color_head'>{countObj?.cls}</span>  <span >Jobs</span> <br></br> for <span >Colorstone </span> bagging.
-                                    </div> */}
                                     <div className='countBox'>{countObj?.cls}</div>
                                         <div>Jobs Are Ready To Print</div>
                                     <div className='p-1 d-flex justify-content-center align-items-center pb-3'>
@@ -187,9 +332,6 @@ const MultiPart = ({ queries, headers }) => {
                             <div style={{margin:'1rem', boxSizing:'border-box'}} className='bg-white mx-0'>
                                 <Box className="border rounded box_css_mlt d-flex flex-column border_color_head justify-content-between align-items-center m-0">
                                     <div className='w-100 d-flex justify-content-center align-items-center py-2 border-bottom border_color_head'><Typography sx={{fontWeight:'bold'}}>MISC BAGS</Typography></div>
-                                    {/* <div className='p-2 text-break text-center'>
-                                            Your Selection having <span className='number-box border border_color_head'>{countObj?.misc}</span>  <span >Jobs</span> <br></br> for <span >Misc </span> bagging.
-                                    </div> */}
                                     <div className='countBox'>{countObj?.misc}</div>
                                         <div>Jobs Are Ready To Print</div>
                                     <div className='p-1 d-flex justify-content-center align-items-center pb-3'>
@@ -200,9 +342,6 @@ const MultiPart = ({ queries, headers }) => {
                             <div style={{margin:'1rem', boxSizing:'border-box'}} className='bg-white mx-0'>
                                 <Box className="border rounded box_css_mlt d-flex flex-column border_color_head justify-content-between align-items-center m-0">
                                     <div className='w-100 d-flex justify-content-center align-items-center py-2 border-bottom border_color_head'><Typography sx={{fontWeight:'bold'}}>FINDING BAGS</Typography></div>
-                                    {/* <div className='p-2 text-break text-center'>
-                                            Your Selection having <span className='number-box border border_color_head'>{countObj?.finding}</span>  <span >Jobs</span> <br></br> for <span >Finding </span> bagging.
-                                    </div> */}
                                     <div className='countBox'>{countObj?.finding}</div>
                                         <div>Jobs Are Ready To Print</div>
                                     <div className='p-1 d-flex justify-content-center align-items-center pb-3'>
@@ -210,58 +349,6 @@ const MultiPart = ({ queries, headers }) => {
                                     </div>
                                 </Box>
                             </div>
-                        {/* <div style={{ margin: "1rem" }}>
-                            <Box className="diaCount diaextra">{countObj.dia}</Box>
-                            <Button
-                                color="success"
-                                variant="contained"
-                                sx={{ fontWeight: "bold", backgroundColor: `#28C76F`, textTransform: "none", minWidth:'120px' }}
-                                className='succ_btn'
-                                disabled={countObj.dia < 1}
-                                onClick={() => handleButtonClick('diamond')}
-                            >
-                                {console.log(countObj.dia)}
-                                <Typography sx={{color:'white', fontWeight:'bold'}}>Diamond</Typography>
-                            </Button>
-                        </div>
-                        <div style={{ margin: "1rem" }}>
-                            <Box className="diaCount diaextra1">{countObj.cls}</Box>
-                            <Button
-                                color="success"
-                                variant="contained"
-                                sx={{ fontWeight: "bold", backgroundColor: `#EA5455`, textTransform: "none", minWidth:'120px' }} className='cls_btn'
-                                disabled={countObj.cls < 1}
-                                onClick={() => handleButtonClick('colorstone')}
-                            >
-                                <Typography sx={{color:'white', fontWeight:'bold'}}>ColorStone</Typography>
-                            </Button>
-                        </div>
-                        <div style={{ margin: "1rem" }}>
-                            <Box className="diaCount diaextra2">{countObj.misc}</Box>
-                            <Button
-                                color="success"
-                                variant="contained"
-                                sx={{ fontWeight: "bold", backgroundColor: `#FF9F43`, textTransform: "none", minWidth:'120px' }}
-                                className='misc_btn'
-                                disabled={countObj.misc < 1}
-                                onClick={() => handleButtonClick('misc')}
-                            >
-                                <Typography sx={{color:'white', fontWeight:'bold'}}>Misc</Typography>
-                            </Button>
-                        </div>
-                        <div style={{ margin: "1rem" }}>
-                            <Box className="diaCount diaextra3">{countObj.finding}</Box>
-                            <Button
-                                color="success"
-                                variant="contained"
-                                sx={{ fontWeight: "bold", backgroundColor: `#655BD3`, textTransform: "none", minWidth:'120px' }}
-                                className='find_btn'
-                                disabled={countObj.finding < 1}
-                                onClick={() => handleButtonClick('finding')}
-                            >
-                                <Typography sx={{color:'white', fontWeight:'bold'}}>Finding</Typography>
-                            </Button>
-                        </div> */}
                         </div>
                     </div>
                 </div>
@@ -284,16 +371,11 @@ const MultiPart = ({ queries, headers }) => {
                             { e?.data?.rd?.CustomerCode?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.CustomerCode}</div>}
                             { e?.data?.rd?.OrderNo?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.OrderNo}</div>}
                             { e?.data?.rd?.promiseDatef?.length > 0 && <div className='fsjbsbg fw-bold'>{e?.data?.rd?.promiseDatef}</div>}
-                            {/* { e?.data?.rd?.mastermanagement_maketypename?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.mastermanagement_maketypename}</div>} */}
                             { e?.data?.rd?.Size?.length > 0 && <div className='fsjbsbg'>Size: {e?.data?.rd?.Size}</div>}
                             { e?.data?.rd?.MetalType?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.MetalType?.split(" ")[1] + " " +e?.data?.rd?.MetalColorCo}</div>}
                             { e?.data?.rd?.MetalWeight > 0 && <div className='fsjbsbg'>{(+e?.data?.rd?.MetalWeight)?.toFixed(3)}gm</div>}
-                            {/* <div className='fsjbsbg'>{(+e?.data?.rd?.MetalWeight)?.toFixed(3)}gm</div>
-                            { e?.data?.rd?.Size?.length > 0 && <div className='fsjbsbg'>Size: {e?.data?.rd?.Size}</div>} */}
                             <div className='d-flex justify-content-start align-items-center'>
-                            <QRCodeGenerator
-                              text={e?.data?.rd.serialjobno}
-                            />
+                                <QRCodeGenerator text={e?.data?.rd.serialjobno} />
                             </div>
                           </div>
                             )
@@ -306,15 +388,10 @@ const MultiPart = ({ queries, headers }) => {
                             { e?.data?.rd?.CustomerCode?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.CustomerCode}</div>}
                             { e?.data?.rd?.OrderNo?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.OrderNo}</div>}
                             { e?.data?.rd?.promiseDatef?.length > 0 && <div className='fsjbsbg fw-bold'>{e?.data?.rd?.promiseDatef}</div>}
-                            {/* { e?.data?.rd?.mastermanagement_maketypename?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.mastermanagement_maketypename}</div>} */}
                             { e?.data?.rd?.Size?.length > 0 && <div className='fsjbsbg'>Size: {e?.data?.rd?.Size}</div>}
                             { e?.data?.rd?.MetalType?.length > 0 && <div className='fsjbsbg'>{e?.data?.rd?.MetalType?.split(" ")[1] +" " +e?.data?.rd?.MetalColorCo}</div>}
-                            {/* { e?.data?.rd?.MetalWeight > 0 && <div className='fsjbsbg'>{(+e?.data?.rd?.MetalWeight)?.toFixed(3)}gm</div>}
-                            { e?.data?.rd?.Size?.length > 0 && <div className='fsjbsbg'>Size: {e?.data?.rd?.Size}</div>} */}
                             <div className='d-flex justify-content-start align-items-center'>
-                            <QRCodeGenerator
-                              text={e?.data?.rd.serialjobno}
-                            />
+                                <QRCodeGenerator text={e?.data?.rd.serialjobno} />
                             </div>
                           </div>
                         }
