7a04209ef4ef6c42f28cfe30337d1568259fbd9c
krunal <krunal@gmail.com.>
2024-12-11 11:45:39 +0530

    one function logic change in kpi dashboard

--- Desktop/inovicegit/src/pages/dashboard/componentsofkpi/components/global.js
+++ Desktop/inovicegit/src/pages/dashboard/componentsofkpi/components/global.js
@@ -40,20 +40,38 @@ export const checkDivByZero = (data) => {
   }
 } 
 
+// export const makeWordShort = (val) => {
+//   // Check for null, undefined, or empty value
+//   if (val === null || val === undefined || val === 'null' || val === 'undefined' || val === '') {
+//     return '';
+//   } else {
+//     // Check for "customer" and "manufacturer" and return short form
+//     if (val.toLowerCase().includes('customer')) {
+//       return 'Cust.';
+//     }
+//     if (val.toLowerCase().includes('manufacturer')) {
+//       return 'MFG';
+//     }
+
+//     // If none of the conditions match, return the original value
+//     return val;
+//   }
+// };
 export const makeWordShort = (val) => {
+  try {
+  
   // Check for null, undefined, or empty value
   if (val === null || val === undefined || val === 'null' || val === 'undefined' || val === '') {
     return '';
   } else {
-    // Check for "customer" and "manufacturer" and return short form
-    if (val.toLowerCase().includes('customer')) {
-      return 'Cust.';
-    }
-    if (val.toLowerCase().includes('manufacturer')) {
-      return 'MFG';
-    }
+    // Replace 'Customer' with 'Cust.' and 'Manufacturer' with 'MFG'
+    const shortVal = val
+      .replace(/Customer/g, 'Cust.')
+      .replace(/Manufacturer/g, 'MFG');
 
-    // If none of the conditions match, return the original value
-    return val;
+    return shortVal;
   }
+} catch (error) {
+  console.log(error);
+}
 };
